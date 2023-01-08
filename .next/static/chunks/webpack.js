483aa1e958d09ddb5da199b93dca564e3c78a44d
Armandilo <muhammadarmand@student.usm.my>
2023-01-09 01:54:27 +0800

    Navigation added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c86a35a58d1ebcad"; }
+/******/ 		__webpack_require__.h = function() { return "dc312f1e2a661fd5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -2,23 +2,23 @@ import React from 'react'
 import styles from "../styles/Navbar.module.css"
 import Image from 'next/image'
 const Navbar = () => {
+ 
   return (
     <div className={styles.container}>
       <div className={styles.item}>
-
       </div>
       <div className={styles.logoContainer}>
       <div className={styles.item}>
         </div>
         <ul className = {styles.list}>
-          <li className = {styles.listitem}>Home</li>
+          <li className = {styles.listitem}><a href="javascript:window.location='/';">Home</a></li>
           <li className = {styles.listitem}>Order</li>
           <li className = {styles.listitem}>Contact</li>
         </ul>
       </div>
       <div className={styles.item}>
         <div className ={styles.cart}>
-          <Image src ="/img/shopping-cart.png" alt="" width="30" height="30"/>
+         <a href="cart"><Image src ="/img/shopping-cart.png" alt="" width="30" height="30"/></a> 
           
         </div>
         <div className = {styles.counter}>2</div>
